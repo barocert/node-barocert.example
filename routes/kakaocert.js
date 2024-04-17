5ed877727daee2c4f4a28b629c8e2becb037a94d
정우석 <wsj@linkhubcorp.com>
2024-04-17 12:06:11 +0900

    주석 수정

--- routes/kakaocert.js
+++ routes/kakaocert.js
@@ -1,3 +1,21 @@
+/*
+* Barocert KAKAO API Node SDK Example
+*
+* 업데이트 일자 : 2024-04-17
+* 연동기술지원 연락처 : 1600-9854
+* 연동기술지원 이메일 : code@linkhubcorp.com
+*         
+* <테스트 연동개발 준비사항>
+*   1) API Key 변경 (연동신청 시 메일로 전달된 정보)
+*       - LinkID : 링크허브에서 발급한 링크아이디
+*       - SecretKey : 링크허브에서 발급한 비밀키
+*   2) ClientCode 변경 (연동신청 시 메일로 전달된 정보)
+*       - ClientCode : 이용기관코드 (파트너 사이트에서 확인가능)
+*   3) SDK 환경설정 필수 옵션 설정
+*       - IPRestrictOnOff : 인증토큰 IP 검증 설정, true-사용, false-미사용, (기본값:true)
+*       - UseStaticIP : 통신 IP 고정, true-사용, false-미사용, (기본값:false)
+*/
+
 var express = require('express');
 var router = express.Router();
 var kakaocert = require('barocert');
@@ -9,10 +27,10 @@ kakaocert.config({
   // 비밀키
   SecretKey: 'SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I=',
 
-  // 인증토큰 IP제한기능 사용여부, true-사용, false-미사용, 기본값(true)
+  // 인증토큰 IP 검증 설정, true-사용, false-미사용, (기본값:true)
   IPRestrictOnOff: true,
 
-  // 카카오써트 API 서비스 고정 IP 사용여부, true-사용, false-미사용, 기본값(false)
+  // 통신 IP 고정, true-사용, false-미사용, (기본값:false)
   UseStaticIP: false,
 
   defaultErrorHandler: function (Error) {
